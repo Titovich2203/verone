ee49a73e28ff07e8c0159be7e4d1a2c0a3b440e7
Titovich2203 <titovich2203@gmail.com>
2021-03-16 23:29:23 +0000

    first commit

---
+++ src/main/java/sn/verone/domain/User.java
@@ -0,0 +1,14 @@
+package sn.verone.domain;
+
+public class User {
+    private int id;
+    private String numCarte;
+    private String nationalite;
+    private String email;
+    private String password;
+    private String photo;
+    private Profil nomComplet;
+    private Profil adresse;
+    private Profil profil;
+
+}
